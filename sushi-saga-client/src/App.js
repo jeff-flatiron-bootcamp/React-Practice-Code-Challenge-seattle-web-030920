59656928fc38eae673d51ba3a722b20dd27430ef
jeff_simon <jeffsbillinfo@gmail.com>
2020-05-17 21:19:45 -0700

    practice2 work

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,75 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    eatenSushi: [],
+    displayIndex: 0,
+    usersWallet: 100
+  }
+
+  componentDidMount()
+  {
+    fetch(API)
+    .then(res => res.json())
+    .then(json => this.setState({
+      sushis: json
+    }))
+  }
+
+  eatSushi = (sushiToEat) => {
+    //console.log(`called eatSushi with ${sushiToEat.name}`)
+    if(this.state.usersWallet >= sushiToEat.price)
+    {
+      if(!this.state.eatenSushi.includes(sushiToEat))
+      {
+        this.setState({
+          eatenSushi: [...this.state.eatenSushi, sushiToEat],
+          usersWallet: this.state.usersWallet - sushiToEat.price
+        })
+      }
+    }
+  }
+
+  getCurrentFourSushi = () => {
+    console.log(`getCurrentFourSushi, value of display index = ${this.state.displayIndex}`)
+    return this.state.sushis.slice(this.state.displayIndex, this.state.displayIndex+4)
+  }
+
+  nextFourSushi = () => {
+      //console.log("nextFourSushi")
+    let updatedIndex = this.state.displayIndex +4    
+    updatedIndex = updatedIndex > this.state.sushis.length-1 ? 0 : updatedIndex
+    console.log(`getCurrentFourSushi, value of updatedIndex= ${updatedIndex}`)
+    this.setState({
+      displayIndex: updatedIndex
+    })
+  }
+
+  addUserFunds = (event) =>
+  {
+    event.preventDefault()
+    let inputFunds = parseInt(event.currentTarget.children[0].value)
+    inputFunds = inputFunds ? inputFunds : 0
+    console.log(`${inputFunds}`)
+    this.setState({
+      usersWallet: inputFunds+this.state.usersWallet
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <form onSubmit={this.addUserFunds}>
+        Add $ to Budget
+          <input type="text" />
+          <input type="submit" />
+        </form>
+        <SushiContainer displaySushis={this.getCurrentFourSushi()}
+          more={this.nextFourSushi}
+          eatenSushi={this.state.eatenSushi}
+          eatSushi={this.eatSushi}/>
+        <Table usersBalance={this.state.usersWallet} emptyPlates={this.state.eatenSushi}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
@@ -9,8 +10,16 @@ const SushiContainer = (props) => {
           /* 
              Render Sushi components here!
           */
+          props.displaySushis.map((sushi) => {
+            return <Sushi sushiToDisplay={sushi} 
+            key={sushi.id} 
+            eatenStatus={props.eatenSushi.includes(sushi)}
+            eatSushi={props.eatSushi}
+            />
+          })
+
         }
-        <MoreButton />
+        <MoreButton more={props.more}/>
       </div>
     </Fragment>
   )
